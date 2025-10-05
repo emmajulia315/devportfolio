2136f347ee95335235199976401dffbe200d3d9e
emmajulia315 <emmajulia315@gmail.com>
2025-10-05 10:46:53 -0700

    projects add

--- sections/ProjectSection.tsx
+++ sections/ProjectSection.tsx
@@ -8,11 +8,11 @@ import { useSection } from "context/section";
 import useOnScreen from "hooks/useOnScreen";
 import useScrollActive from "hooks/useScrollActive";
 
-// import terminalPortfolio from "public/projects/terminal-portfolio.webp";
-// import haruFashion from "public/projects/haru-fashion.webp";
-// import haruApi from "public/projects/haru-api.webp";
-// import astroPaper from "public/projects/astro-paper.webp";
-// import nextBookstore from "public/projects/next-bookstore.webp";
+import terminalPortfolio from "public/projects/terminal-portfolio.webp";
+import haruFashion from "public/projects/haru-fashion.webp";
+import haruApi from "public/projects/haru-api.webp";
+import astroPaper from "public/projects/astro-paper.webp";
+import nextBookstore from "public/projects/next-bookstore.webp";
 import shadcnAdmin from "public/projects/shadcn-admin.webp";
 import Mypop from "public/projects/mypop.png";
 import Radio from "public/projects/radio.png";
@@ -29,6 +29,7 @@ import gsmplayer from "public/projects/gsmplayer.png";
 import Maalmate from "public/projects/maalmate.png";
 import ZadeyCosmogems from "public/projects/zadey.png";
 import portfolio from "public/projects/image.png";
+import trivohgmail from "public/projects/trivohgmail.png";
 
 
 
@@ -368,101 +369,120 @@ const projects = [
     bgColor: "bg-[#9FD0E3]",
     githubApi: "https://api.github.com/repos/satnaing/astro-paper",
   },
-  // {
-  //   title: "AstroPaper",
-  //   type: "Frontend",
-  //   image: (
-  //     <Image
-  //       src={astroPaper}
-  //       sizes="100vw"
-  //       fill
-  //       alt="AstroPaper"
-  //       className="transition-transform duration-500 hover:scale-110 object-cover"
-  //     />
-  //   ),
-  //   desc: "A minimal, accessible and SEO-friendly Astro blog theme. One of the most starred blog templates built with Astro.",
-  //   tags: ["Astro", "TypeScript", "React", "TailwindCSS"],
-  //   liveUrl: "https://astro-paper.pages.dev/",
-  //   codeUrl: "https://github.com/satnaing/astro-paper",
-  //   bgColor: "bg-[#9FD0E3]",
-  //   githubApi: "https://api.github.com/repos/satnaing/astro-paper",
-  // },
-  // {
-  //   title: "Terminal Portfolio",
-  //   type: "Frontend",
-  //   image: (
-  //     <Image
-  //       src={terminalPortfolio}
-  //       sizes="100vw"
-  //       fill
-  //       alt="Terminal Portfolio"
-  //       className="transition-transform duration-500 hover:scale-110 object-cover"
-  //     />
-  //   ),
-  //   desc: "My portfolio website in terminal version developed with React and TypeScript. ",
-  //   tags: ["React", "TypeScript", "Styled-Components"],
-  //   liveUrl: "https://terminal.satnaing.dev/",
-  //   codeUrl: "https://github.com/satnaing/terminal-portfolio",
-  //   bgColor: "bg-[#B4BEE0]",
-  //   githubApi: "https://api.github.com/repos/satnaing/terminal-portfolio",
-  // },
-  // {
-  //   title: "Haru Fashion",
-  //   type: "Frontend",
-  //   image: (
-  //     <Image
-  //       src={haruFashion}
-  //       sizes="100vw"
-  //       fill
-  //       alt="Haru Fashion App"
-  //       className="transition-transform duration-500 hover:scale-110 object-cover"
-  //     />
-  //   ),
-  //   desc: "An ecommerce web application where users can browse various products, add to wishlist, add to cart, and make purchase. Available in English and Burmese languages.",
-  //   tags: ["NextJS", "TypeScript", "TailwindCSS", "ContextAPI"],
-  //   liveUrl: "https://haru-fashion.vercel.app/",
-  //   codeUrl: "https://github.com/satnaing/haru-fashion",
-  //   bgColor: "bg-[#A6CECE]",
-  //   githubApi: "https://api.github.com/repos/satnaing/haru-fashion",
-  // },
-  // {
-  //   title: "Haru API",
-  //   type: "Backend",
-  //   image: (
-  //     <Image
-  //       src={haruApi}
-  //       sizes="100vw"
-  //       fill
-  //       alt="Haru API"
-  //       className="transition-transform duration-500 hover:scale-110 object-cover"
-  //     />
-  //   ),
-  //   desc: "A RESTful API developed for Haru fashion ecommerce project. Include CRUD operations, authentication, authorization, forgot/reset password and full-text search.",
-  //   tags: ["ExpressJS", "TypeScript", "PostgreSQL", "Prisma"],
-  //   liveUrl: "https://satnaing.github.io/haru-api/",
-  //   codeUrl: "https://github.com/satnaing/haru-api",
-  //   bgColor: "bg-[#C5E4E7]",
-  //   githubApi: "https://api.github.com/repos/satnaing/haru-api",
-  // },
-  // {
-  //   title: "Next Bookstore",
-  //   type: "Frontend + HeadlessCMS",
-  //   image: (
-  //     <Image
-  //       src={nextBookstore}
-  //       sizes="100vw"
-  //       fill
-  //       alt="Next Bookstore"
-  //       className="transition-transform duration-500 hover:scale-110 object-cover"
-  //     />
-  //   ),
-  //   desc: "An online bookstore developed using NextJS 13 with appDir and StrapiCMS.",
-  //   tags: ["NextJS", "Radix UI ", "TailwindCSS", "TanstackQuery", "StrapiCMS"],
-  //   liveUrl: "https://nextbookstore.vercel.app/",
-  //   codeUrl: "https://github.com/satnaing/next-bookstore",
-  //   bgColor: "bg-[#EBF4F4]",
-  //   githubApi: "https://api.github.com/repos/satnaing/next-bookstore",
-  // },
+  {
+    title: "Trivoh | Gmail App",
+    type: "Frontend",
+    image: (
+      <Image
+        src={trivohgmail}
+        sizes="100vw"
+        fill
+        alt="Zpl"
+        className="transition-transform duration-500 hover:scale-110 object-cover"
+      />
+    ),
+    desc: "Trivoh | Gmail App",
+    tags: ["Trivoh | Mailer App", "Typescript","Next-theme","Next-auth","Tiptap editor", "Resend Mails","Full-stack", "TailwindCSS"],
+    liveUrl: "https://trivohgmail.vercel.app/",
+    codeUrl: "https://github.com/satnaing/astro-paper",
+    bgColor: "bg-[#9FD0E3]",
+    githubApi: "https://api.github.com/repos/satnaing/astro-paper",
+  },
+  {
+    title: "AstroPaper",
+    type: "Frontend",
+    image: (
+      <Image
+        src={astroPaper}
+        sizes="100vw"
+        fill
+        alt="AstroPaper"
+        className="transition-transform duration-500 hover:scale-110 object-cover"
+      />
+    ),
+    desc: "A minimal, accessible and SEO-friendly Astro blog theme. One of the most starred blog templates built with Astro.",
+    tags: ["Astro", "TypeScript", "React", "TailwindCSS"],
+    liveUrl: "https://astro-paper.pages.dev/",
+    codeUrl: "https://github.com/satnaing/astro-paper",
+    bgColor: "bg-[#9FD0E3]",
+    githubApi: "https://api.github.com/repos/satnaing/astro-paper",
+  },
+  {
+    title: "Terminal Portfolio",
+    type: "Frontend",
+    image: (
+      <Image
+        src={terminalPortfolio}
+        sizes="100vw"
+        fill
+        alt="Terminal Portfolio"
+        className="transition-transform duration-500 hover:scale-110 object-cover"
+      />
+    ),
+    desc: "My portfolio website in terminal version developed with React and TypeScript. ",
+    tags: ["React", "TypeScript", "Styled-Components"],
+    liveUrl: "https://terminal.satnaing.dev/",
+    codeUrl: "https://github.com/satnaing/terminal-portfolio",
+    bgColor: "bg-[#B4BEE0]",
+    githubApi: "https://api.github.com/repos/satnaing/terminal-portfolio",
+  },
+  {
+    title: "Haru Fashion",
+    type: "Frontend",
+    image: (
+      <Image
+        src={haruFashion}
+        sizes="100vw"
+        fill
+        alt="Haru Fashion App"
+        className="transition-transform duration-500 hover:scale-110 object-cover"
+      />
+    ),
+    desc: "An ecommerce web application where users can browse various products, add to wishlist, add to cart, and make purchase. Available in English and Burmese languages.",
+    tags: ["NextJS", "TypeScript", "TailwindCSS", "ContextAPI"],
+    liveUrl: "https://haru-fashion.vercel.app/",
+    codeUrl: "https://github.com/satnaing/haru-fashion",
+    bgColor: "bg-[#A6CECE]",
+    githubApi: "https://api.github.com/repos/satnaing/haru-fashion",
+  },
+  {
+    title: "Haru API",
+    type: "Backend",
+    image: (
+      <Image
+        src={haruApi}
+        sizes="100vw"
+        fill
+        alt="Haru API"
+        className="transition-transform duration-500 hover:scale-110 object-cover"
+      />
+    ),
+    desc: "A RESTful API developed for Haru fashion ecommerce project. Include CRUD operations, authentication, authorization, forgot/reset password and full-text search.",
+    tags: ["ExpressJS", "TypeScript", "PostgreSQL", "Prisma"],
+    liveUrl: "https://satnaing.github.io/haru-api/",
+    codeUrl: "https://github.com/satnaing/haru-api",
+    bgColor: "bg-[#C5E4E7]",
+    githubApi: "https://api.github.com/repos/satnaing/haru-api",
+  },
+  {
+    title: "Next Bookstore",
+    type: "Frontend + HeadlessCMS",
+    image: (
+      <Image
+        src={nextBookstore}
+        sizes="100vw"
+        fill
+        alt="Next Bookstore"
+        className="transition-transform duration-500 hover:scale-110 object-cover"
+      />
+    ),
+    desc: "An online bookstore developed using NextJS 13 with appDir and StrapiCMS.",
+    tags: ["NextJS", "Radix UI ", "TailwindCSS", "TanstackQuery", "StrapiCMS"],
+    liveUrl: "https://nextbookstore.vercel.app/",
+    codeUrl: "https://github.com/satnaing/next-bookstore",
+    bgColor: "bg-[#EBF4F4]",
+    githubApi: "https://api.github.com/repos/satnaing/next-bookstore",
+  },
   {
     title: "Shadcn Admin",
     type: "Backend",
